bb2b97bb7065fb959318e544b1a51efdfb532851
Karakat Almasova <karakatalmasova@Karakats-MacBook-Air.local>
2023-07-14 20:21:34 +0600

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-o&z)^w48xbg%9eldd5qlqhuxt6#6pnvl7xmkpg$e8f6j&p*xr%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
