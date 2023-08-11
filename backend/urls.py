2e27cf8355aaec4884c9b5fbcc19daa82490e1ba
Karakat Almasova <karakatalmasova@Karakats-MacBook-Air.local>
2023-08-11 11:06:20 +0600

    api error

--- backend/urls.py
+++ backend/urls.py
@@ -4,8 +4,6 @@ from api import views
 urlpatterns = [
     path("process_image", views.process_image),
     path("chat_with_chatgpt", views.chat_with_chatgpt),
-    path("search_person", views.search_person),
-
     # path("process_audio", views.process_audio),
     # path('register', views.register),
     # path('get_result', views.get_result),
